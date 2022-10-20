a7ef65226a63733f5b501c724ffa5465c1b064c5
Emilse Arias <emilsearias@gmail.com>
2022-10-20 18:49:45 -0300

    agregamos la conexion a la base de datos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from 'cors';
 import path from 'path';
+import './database'
 //crear una instancia de express
 const app = express();
 
