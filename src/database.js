d25002c024d541bf5f4675e818e43c2b315b6ed2
Emilse Arias <emilsearias@gmail.com>
2022-11-01 18:56:25 -0300

    agregamos la direccion de la BD de produccion

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 // localhost: 127.0.0.1
-const url = 'mongodb://localhost:27017/cafe-branch';
+// const url = 'mongodb://localhost:27017/cafe-branch'; //BD-local
+// const url = 'mongodb+srv://nunicho:jnup@MjxQwNP2Q4@cluster0.b8sgvyv.mongodb.net/cafe-branch';
+const url = 'mongodb+srv://emi:emi2020@cluster0.xgqs3sk.mongodb.net/cafe-branch-c6i';
 
 mongoose.connect(url);
 
